f5ecc1288457c28a2d5c163df9957fa6490130e1
SalmanSaeed24a <95068089+SalmanSaeed24a@users.noreply.github.com>
2021-11-30 11:51:44 +0500

    Add files via upload

---
+++ evanorodd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+int a;
+printf("enter the number a");
+scanf("%d",&a);
+if(a%2==0){
+printf("%d is the odd number \n :",a);}
+else{
+printf("%d is the even number \n :",a);}
+}
